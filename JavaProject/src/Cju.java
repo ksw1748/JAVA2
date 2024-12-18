7625875ab75245f8249500c3b345b9717feac416
ksw1748 <ksw1748@gmail.com>
2024-12-18 22:33:12 +0900

    프로그램 창 구현

--- JavaProject/src/Cju.java
+++ JavaProject/src/Cju.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
 
-public class Cju {
+public class Cju extends JFrame {
+	public Cju() {
+		setTitle("캠퍼스 식당 메뉴 및 대기 시간 알리미");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
 
+	public static void main(String[] args) {
+		new Cju();
+
+	}
 }
