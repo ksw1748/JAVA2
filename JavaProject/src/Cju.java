dfe2ec30956456ed48baf3704cefd2f7e83dfe0f
ksw1748 <ksw1748@gmail.com>
2024-12-18 22:39:32 +0900

    식당의 선택,메뉴,대기시간의 컴포넌트 추가

--- JavaProject/src/Cju.java
+++ JavaProject/src/Cju.java
@@ -1,7 +1,14 @@
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Cju extends JFrame {
+	private JComboBox<String> restaurantComboBox;
+	private JComboBox<String> menuComboBox;
+	private JLabel waiting;
+
 	public Cju() {
+
 		setTitle("캠퍼스 식당 메뉴 및 대기 시간 알리미");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400, 300);
