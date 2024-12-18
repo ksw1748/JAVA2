ef64814bbf11ce63995b99eb7c3c43d16d7bfbfc
ksw1748 <ksw1748@gmail.com>
2024-12-19 00:59:59 +0900

    리스너 추가, 메뉴 클래스 추가

--- JavaProject/src/Cju.java
+++ JavaProject/src/Cju.java
@@ -1,4 +1,7 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -8,17 +11,23 @@ public class Cju extends JFrame {
 	private String restaurant = "학교 식당";
 	private JComboBox<String> menuComboBox;
 	private JLabel waiting;
-	
+	private static ArrayList<Menu> menuData = new ArrayList<>();
 
 	public Cju() {
-		
+
 		menuComboBox = new JComboBox<>();
-        add(new JLabel("메뉴 선택:"));
-        add(menuComboBox);
-        
-        waiting = new JLabel("대기 시간: ");
-        add(waiting);
-        
+		add(new JLabel("메뉴 선택:"));
+		add(menuComboBox);
+
+		waiting = new JLabel("대기 시간: ");
+		add(waiting);
+		
+		menuComboBox.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+				 
+			 }
+			
+		});
 
 		setTitle("캠퍼스 식당 메뉴 및 대기 시간 알리미");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,4 +40,9 @@ public class Cju extends JFrame {
 		new Cju();
 
 	}
+}
+
+class Menu {
+	private String name;
+	private int waiting;
 }
